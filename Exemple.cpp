a1cd9c8a35e255e2198061afb6a113de07aee5bb
gabrielvillarart <gabrielvillarsqui@gmail.com>
2024-04-25 23:10:39 -0300

    Fix full precision types and signed operations.

--- Exemple.cpp
+++ Exemple.cpp
@@ -5,9 +5,10 @@
 
 int main() 
 {
-	using fixed_type = fixed32;
+	using fixed_type = fixed16;
 
-	constexpr auto point = fixed_type::local_decimals[5];
+	constexpr auto one = fixed_type::half;
+	//constexpr auto pointr = point / 10;
 
 	constexpr auto max = fixed_type::max();
 	constexpr int64 max_i = max;
@@ -17,11 +18,11 @@ int main()
 	constexpr int64 min_i = min;
 	constexpr double min_d = min;
 	
-	CONSTEXPR fixed_type a = "1";
+	CONSTEXPR fixed_type a = "6";
 	CONSTEXPR int64 a_i = a;
 	CONSTEXPR double a_d = a;
 	
-	CONSTEXPR fixed_type b = ".25";
+	CONSTEXPR fixed_type b = "0.25";
 	CONSTEXPR int64 b_i = b;
 	CONSTEXPR double b_d = b;
 	
--- Source/Fixed.h
+++ Source/Fixed.h
@@ -2,34 +2,22 @@
 
 #include "Core.h"
 
-constexpr static uint64 cached_decimals[] = 
+struct fixed_utils
 {
-	0,
-	1844674407370955162,
-	3689348814741910323,
-	5534023222112865485,
-	7378697629483820646,
-	9223372036854775808,
-	11068046444225730970,
-	12912720851596686131,
-	14757395258967641293,
-	16602069666338596454,
-};
-
-constexpr static uint64 GetDecimalFor(uint64 resolution, uint64 digit_value)
-{
-	if (resolution < 64)
+	constexpr static uint64 cached_decimals[] = 
 	{
-		uint64 shift = 64-resolution;
-		uint64 half = uint64(1) << (shift - 1);
-		return (cached_decimals[digit_value] + half) >> shift;
-	}
-
-	return cached_decimals[digit_value];
-}
-
-
-
+		0,
+		1844674407370955162,
+		3689348814741910323,
+		5534023222112865485,
+		7378697629483820646,
+		9223372036854775808,
+		11068046444225730970,
+		12912720851596686131,
+		14757395258967641293,
+		16602069666338596454,
+	};
+};
 
 template<typename underlying_type, underlying_type precision>
 class fixed
@@ -49,19 +37,7 @@ public:
 	constexpr static underlying_type whole_mask = underlying_type(-1) << precision;
 	constexpr static underlying_type decimals_mask = ~whole_mask;
 	constexpr static uint64 one = (uint64)(decimals_mask) + (uint64)1;
-	constexpr static uint64 local_decimals[] = 
-	{
-		0,
-		GetDecimalFor(precision, 1),
-		GetDecimalFor(precision, 2),
-		GetDecimalFor(precision, 3),
-		GetDecimalFor(precision, 4),
-		GetDecimalFor(precision, 5),
-		GetDecimalFor(precision, 6),
-		GetDecimalFor(precision, 7),
-		GetDecimalFor(precision, 8),
-		GetDecimalFor(precision, 9),
-	};
+	constexpr static uint64 half = one / 2;
 	
 	constexpr static uint64 wholes_precision = (sizeof(underlying_type) * 8) - precision;
 	constexpr static underlying_type half_precision = precision / 2;
@@ -79,8 +55,8 @@ public:
 	template<uint32 string_length>
 	constexpr fixed(string_literal<string_length> string)
 	{
-		underlying_type_unsigned wholes = 0;
-		underlying_type_unsigned decimals = 0;
+		uint64 wholes = 0;
+		uint64 decimals = 0;
 
 		size_t string_end = string_length - 1;
 		size_t point_index = 0;
@@ -99,7 +75,7 @@ public:
 			}
 			else if (character >= '0' && character <= '9')
 			{
-				underlying_type character_value = (underlying_type)(character - '0');
+				uint64 character_value = (uint64)(character - '0');
 				wholes = wholes * 10 + character_value;
 			}
 			else if (i == 0 && character == '-')
@@ -129,7 +105,7 @@ public:
 				if (character >= '0' && character <= '9')
 				{
 					char character_value = (char)(character - '0');
-					decimals += local_decimals[character_value] / divisor;
+					decimals += fixed_utils::cached_decimals[character_value] / divisor;
 					divisor *= (uint64)10;
 				}
 				else // Invalid Input.
@@ -139,50 +115,28 @@ public:
 				}
 			}
 		}
+		
+		if (precision < 64)
+		{
+			uint64 shift = 64-precision;
+			uint64 half = uint64(1) << (shift - 1);
+			decimals = (decimals + half) >> shift;
+		}
 
 		if constexpr (std::is_signed_v<underlying_type>)
 		{
+
 			if (is_negative) // Negative
 			{
-				underlying_value = *(underlying_type*)&(~(wholes + decimals) + 1);
+				uint64 result = ~(wholes + decimals) + 1;
+				underlying_value = underlying_type(result);
 				return;
 			}
 		}
-		underlying_value = wholes + decimals;
+		underlying_value = underlying_type(wholes + decimals);
 	}
 
-	constexpr fixed operator +(fixed other) const
-	{
-		fixed product;
-		product.underlying_value = underlying_value + other.underlying_value;
-		return product;
-	}
-
-	constexpr fixed operator -(fixed other) const
-	{
-		fixed product;
-		product.underlying_value = underlying_value - other.underlying_value;
-		return product;
-	}
-
-	constexpr fixed operator *(fixed other) const
-	{
-		underlying_type decimals = ((underlying_value * other.underlying_value) >> precision) & ~half_precision_mask;
-		underlying_type overflow = (underlying_value >> half_precision) * (other.underlying_value >> half_precision_plus_remainder);
-
-		fixed product;
-		product.underlying_value = underlying_type(decimals | overflow);
-		return product;
-	}
-
-	constexpr fixed operator /(fixed other) const
-	{
-		fixed product;
-		product.underlying_value = (underlying_value << half_precision) / (other.underlying_value << half_precision) << precision;
-		product.underlying_value &= half_precision_mask;
-		return product;
-	}
-
+public:
 	static constexpr fixed max()
 	{
 		fixed new_fixed;
@@ -196,13 +150,69 @@ public:
 		new_fixed.underlying_value = min_value;
 		return new_fixed;
 	}
+	
+	static constexpr fixed create_directly(underlying_type value)
+	{
+		fixed ret;
+		ret.underlying_value = value;
+		return ret;
+	}
 
+public:
+	constexpr fixed operator+(fixed other) const
+	{
+		using uT = underlying_type_unsigned;
+
+		fixed sum;
+		sum.underlying_value = std::bit_cast<uT>(underlying_value) + std::bit_cast<uT>(other.underlying_value);
+		return sum;
+	}
+
+	constexpr fixed operator-(fixed other) const
+	{
+		using uT = underlying_type_unsigned;
+
+		fixed difference;
+		difference.underlying_value = std::bit_cast<uT>(underlying_value) - std::bit_cast<uT>(other.underlying_value);
+		return difference;
+	}
+
+	constexpr fixed operator*(fixed other) const
+	{
+		//using uT = underlying_type_unsigned;
+		using uT = underlying_type;
+
+		const uT this_value = std::bit_cast<uT>(underlying_value); 
+		const uT other_value = std::bit_cast<uT>(other.underlying_value); 
+
+		uT decimals = this_value * other_value;
+		decimals >>= uT(precision);
+		decimals &= ~half_precision_mask;
+
+		uT overflow = (this_value >> uT(half_precision)) * (other_value >> uT(half_precision_plus_remainder));
+
+		uT ret = decimals | overflow;
+
+		fixed product;
+		product.underlying_value = std::bit_cast<underlying_type>(ret);
+		return product;
+	}
+
+	constexpr fixed operator/(fixed other) const
+	{
+		fixed product;
+		product.underlying_value = (underlying_value << half_precision) / (other.underlying_value << half_precision) << precision;
+		product.underlying_value &= half_precision_mask;
+		return product;
+	}
+	
+public:
 	constexpr operator int32() const
 	{
 		if constexpr (wholes_precision == 0)
 			return 0;
 
-		underlying_type ret = underlying_value + local_decimals[5];
+		underlying_type ret = underlying_value + half;
 		ret = ret >> precision;
 		return int32(ret);
 	}
@@ -212,34 +222,33 @@ public:
 		if constexpr (wholes_precision == 0)
 			return 0;
 
-		//underlying_type ret = underlying_value + local_decimals[5];
-		underlying_type ret = underlying_value >> precision;
+		underlying_type ret = underlying_value + half;
 		return int64(ret);
 	}
 
 	constexpr fixed(double value)
 	{
 		constexpr double cached_conversion_from_double = (double)((uint64)1 << (uint64)precision);
-		underlying_value = value * cached_conversion_from_double;
+		underlying_value = (value * cached_conversion_from_double);
 	}
 
 	constexpr operator double() const
 	{
-		if constexpr (wholes_precision > 0)
-		{
-			underlying_type integer = underlying_value >> precision;
-			underlying_type decimals = underlying_value & decimals_mask;
+		constexpr double cached_conversion_to_double = 0.0625 / (double)((uint64)1 << ((uint64)precision - (uint64)4));
 
-			constexpr double cached_conversion_to_double = 0.0625 / (double)((uint64)1 << ((uint64)precision - (uint64)4));
-			return (double)(integer) + ((double)decimals * cached_conversion_to_double);
-		}
-		else
-		{
-			constexpr double cached_conversion_to_double = 0.0625 / (double)((uint64)1 << ((uint64)precision - (uint64)4));
-			return (double)underlying_value * cached_conversion_to_double;
-		}
+		return (double)underlying_value * cached_conversion_to_double;
+
+		//if constexpr (wholes_precision > 0)
+		//{
+		//	underlying_type integer = underlying_value >> precision;
+		//	underlying_type decimals = underlying_value & decimals_mask;
+
+		//	return (double)(integer) + ((double)decimals * cached_conversion_to_double);
+		//}
+		//else
+		//{
+		//}
 	}
-
 
 private:
 	underlying_type underlying_value = {};
